b06e14e67d2abd3bb4cc946886e904825f7aa67d
YoshuaNava <yoshua.nava.chocron@gmail.com>
2015-09-09 03:11:41 -0430

    Limpiando código viejo.

--- Codigos/Tesis_ws/src/Plane_Segmentation/src/Plane_Segmentation.cpp
+++ Codigos/Tesis_ws/src/Plane_Segmentation/src/Plane_Segmentation.cpp
@@ -1,48 +1,95 @@
- //opencv header files
- #include "ros/ros.h"
- #include "std_msgs/String.h"
- #include "image_transport/image_transport.h"
- #include "sensor_msgs/image_encodings.h"
- #include "cv_bridge/cv_bridge.h"
+//opencv header files
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "image_transport/image_transport.h"
+#include "sensor_msgs/image_encodings.h"
+#include "cv_bridge/cv_bridge.h"
 
- #include "iostream"
- #include "stdio.h" //standard input-output-library
- #include "algorithm" //functions on sequences, like find, sort etc. 
- #include "stdlib.h" //includes random number generation
- #include "time.h" //get date and time information
- #include "math.h"
- #include "string.h" //manipulate strings and arrays
- #include "iomanip"
- #include "fstream"
- #include "vector"
- #include "thread"
- 
- #include "opencv/cv.h"
- #include "opencv/highgui.h"
- #include "opencv/cxcore.h"
- #include "opencv2/highgui/highgui.hpp"
- #include "opencv2/imgproc/imgproc.hpp"
- #include "opencv2/video/tracking.hpp"
- #include "opencv2/core/core.hpp"
- 
- #include "init_structures.h" //initialise structures used in this algorithm
- //#include "img_proc_fcns.h" //various image processing/computer vision functions
- #include "capture.h" //image capture 
- #include "ms_overwrite_safe_buffer.h"
+#include "iostream"
+#include "stdio.h" //standard input-output-library
+#include "algorithm" //functions on sequences, like find, sort etc. 
+#include "stdlib.h" //includes random number generation
+#include "time.h" //get date and time information
+#include "math.h"
+#include "string.h" //manipulate strings and arrays
+#include "iomanip"
+#include "fstream"
+#include "vector"
+#include "thread"
 
- 
- #define Window_W 1.02*proc_W //appriximate wht window width and hight as a function of the frame size
- #define Window_H 1.3*(proc_H)+20
- #define DISPLAY_IMAGE_XY(R,img,X,Y)		if(R){cvNamedWindow(#img); cvMoveWindow(#img, int(round(X*Window_W)), int(round(Y*Window_H))) ;} cv::imshow(#img, img);
- using namespace std;
- using namespace cv;
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+#include "opencv/cxcore.h"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/core/core.hpp"
+
+#include "init_structures.h" //initialise structures used in this algorithm
+//#include "img_proc_fcns.h" //various image processing/computer vision functions
+#include "capture.h" //image capture 
+#include "ms_overwrite_safe_buffer.h"
+
+
+#define Window_W 1.02*proc_W //appriximate wht window width and hight as a function of the frame size
+#define Window_H 1.3*(proc_H)+20
+#define DISPLAY_IMAGE_XY(R,img,X,Y)		if(R){cvNamedWindow(#img); cvMoveWindow(#img, int(round(X*Window_W)), int(round(Y*Window_H))) ;} cv::imshow(#img, img);
+using namespace std;
+using namespace cv;
 
 Mat frame, gray, prevgray, F_hsv, F_YCrCb, F_lab, F_sat, F_hue, F_Cr, F_Cb, F_a, channel_1[4], channel_2[4], channel_3[4], F_iic, src, src_out, dst, histImage, temp_grad[3], sobel[3], F_mag, F_ang, F_lbp; // Mat Declarations
 
 double proc_W, proc_H;
 VideoCapture cap;
 
-void combine_channels(cv::Mat Cr, cv::Mat Cb, cv::Mat a, cv::Mat& iic){
+
+void ShowImages()
+{
+	/*
+	imshow("HSV",F_hsv);
+	//imshow("YCrCb",F_YCrCb);
+	//imshow("Lab",F_lab);
+	imshow("F_hue",F_hue);
+	imshow("F_sat",F_sat);
+	//imshow("F_Cr",F_Cr);
+	//imshow("F_Cb",F_Cb);
+	//imshow("F_a",F_a);
+	imshow("F_iic",F_iic);
+	//imshow("Sobel_Derivative", src_out);
+	//imshow("Histogram", histImage);	
+	imshow("F_mag", F_mag);
+	imshow("F_ang", F_ang);
+	imshow("LBP", F_lbp);
+	//imshow("Prueba", ANG);
+	*/
+	
+	
+	//DISPLAY_IMAGE_XY(false, F_hsv, 0 , 0);
+	//DISPLAY_IMAGE_XY(false, F_YCrCb, 1 , 0);
+	//DISPLAY_IMAGE_XY(false, F_lab, 2 , 0);
+	DISPLAY_IMAGE_XY(true, frame, 0 , 0);
+	cv::resizeWindow("frame", proc_W, proc_H);
+	//DISPLAY_IMAGE_XY(true, gray, 1 , 0);
+	DISPLAY_IMAGE_XY(true, F_hue, 1 , 0);
+	cv::resizeWindow("F_hue", 160, 120);
+	//cv::resizeWindow("F_hue", 160, 120);
+	DISPLAY_IMAGE_XY(true, F_sat, 2 , 0);
+	cv::resizeWindow("F_sat", 160, 120);
+	//cv::resizeWindow("F_sat", 160, 120);
+	//DISPLAY_IMAGE_XY(true, F_Cr, 5 , 0);
+	//DISPLAY_IMAGE_XY(true, F_Cb, 6 , 0);
+	//DISPLAY_IMAGE_XY(true, F_a, 7 , 0);
+	DISPLAY_IMAGE_XY(true, F_iic, 3 , 0);
+	cv::resizeWindow("F_iic", 160, 120);
+	DISPLAY_IMAGE_XY(true, F_mag, 4 , 0);
+	cv::resizeWindow("F_mag", 160, 120);
+	DISPLAY_IMAGE_XY(true, F_ang, 5 , 0);
+	cv::resizeWindow("F_ang", 160, 120);
+	DISPLAY_IMAGE_XY(true, F_lbp, 6 , 0);
+	cv::resizeWindow("F_lbp", 160, 120);
+}
+
+void CalculateIIC(cv::Mat Cr, cv::Mat Cb, cv::Mat a, cv::Mat& iic){
 	
 	//Reference on passing parameters-by-reference in C/C++: http://stackoverflow.com/questions/11235187/opencv-changing-mat-inside-a-function-mat-scope
 	//Reference on adding matrices without saturation: http://answers.opencv.org/question/13769/adding-matrices-without-saturation/
@@ -54,7 +101,41 @@ void combine_channels(cv::Mat Cr, cv::Mat Cb, cv::Mat a, cv::Mat& iic){
 	  iic.convertTo(iic, CV_8UC1);
 }
 
-Mat LBP(Mat img){
+
+
+void CalculateMagnitudeOrientationOfGradients()
+{
+	Scharr(gray, temp_grad[0], gray.depth(), 1, 0, 1, 0, BORDER_DEFAULT);
+	convertScaleAbs(temp_grad[0], sobel[1], 1, 0);
+
+	Scharr(gray, temp_grad[1], gray.depth(), 0, 1, 1, 0, BORDER_DEFAULT);
+	convertScaleAbs(temp_grad[1], sobel[2], 1, 0);
+
+	Mat abs_grad_x = abs(temp_grad[0]);
+	Mat abs_grad_y = abs(temp_grad[1]);
+	F_mag = abs_grad_x + abs_grad_y;
+	F_mag = 255 - F_mag;
+	F_ang = 0*F_mag;
+	float result;
+	//ANG = atan2( temp_grad[1] ,temp_grad[0]);
+	for (int y = 0; y < temp_grad[1].rows; y++) 
+	{
+		for (int x = 0; x < temp_grad[1].cols; x++) 
+		{
+			float valueX = sobel[1].at<uchar>(y,x);
+			float valueY = sobel[2].at<uchar>(y,x);
+			float result = fastAtan2(valueY,valueX);
+			F_ang.at<uchar>(y, x) = (uchar)result;
+		}
+	}
+
+	normalize(F_mag, F_mag, 0, 255, CV_MINMAX);
+	convertScaleAbs(F_mag, F_mag, 1, 0); 
+	normalize(F_ang, F_ang, 0, 255, CV_MINMAX);
+	convertScaleAbs(F_ang, F_ang, 1, 0);	
+}
+
+Mat CalculateLBP(Mat img){
     Mat dst = Mat::zeros(img.rows-2, img.cols-2, CV_8UC1);
     for(int i=1;i<img.rows-1;i++) {
         for(int j=1;j<img.cols-1;j++) {
@@ -74,39 +155,8 @@ Mat LBP(Mat img){
     return dst;
 }
 
-void sobel_derivatives(cv::Mat src, cv::Mat& grad){
 
-    Mat src_gray;
-    int scale = 1;
-    int delta = 0;
-    int ddepth = CV_16S;
-    GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
-
-    /// Convert it to gray 
-    cvtColor( src, src_gray, CV_RGB2GRAY );
-
-    /// Generate grad_x and grad_y
-    Mat grad_x, grad_y;
-    Mat abs_grad_x, abs_grad_y;
-
-    /// Gradient X
-    //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
-    Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-    convertScaleAbs( grad_x, abs_grad_x );
-
-    /// Gradient Y
-    //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-    Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-    convertScaleAbs( grad_y, abs_grad_y );
-
-    /// Total Gradient (approximate)
-    addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
-              
-              
-
-}
-
-void histogram(cv::Mat src, cv::Mat& hist){
+void Histogram(cv::Mat src, cv::Mat& hist){
               
               Mat dst;
               
@@ -159,108 +209,6 @@ void histogram(cv::Mat src, cv::Mat& hist){
 }
 
 
-void InitializeImageVisualizers()
-{
-	//namedWindow( "HSV", 1 ); 
-    //namedWindow( "YCrCb", 1 ); 
-    //namedWindow( "Lab", 1 ); 
-	cv::namedWindow( "frame", CV_WINDOW_NORMAL);
-	cv::resizeWindow("frame", proc_W, proc_H);
-	//namedWindow( "gray", 1 );
-    cv::namedWindow( "F_hue", 0);
-    cv::namedWindow( "F_sat", 0);
-    //namedWindow( "F_Cb", 1 );
-    //namedWindow( "F_Cr", 1 );
-    //namedWindow( "F_a", 1 );
-    cv::namedWindow( "F_iic", 0);
-    //cvNamedWindow( "Histogram", 1);
-    cv::namedWindow( "F_mag", 0);
-    cv::namedWindow( "F_ang", 0);
-    cv::namedWindow( "F_lbp", 0);
-    //cv::namedWindow( "Prueba", 0);
-    //Window Names
-}
-
-
-void ShowImages()
-{
-	/*
-	imshow("HSV",F_hsv);
-	//imshow("YCrCb",F_YCrCb);
-	//imshow("Lab",F_lab);
-	imshow("F_hue",F_hue);
-	imshow("F_sat",F_sat);
-	//imshow("F_Cr",F_Cr);
-	//imshow("F_Cb",F_Cb);
-	//imshow("F_a",F_a);
-	imshow("F_iic",F_iic);
-	//imshow("Sobel_Derivative", src_out);
-	//imshow("Histogram", histImage);	
-	imshow("F_mag", F_mag);
-	imshow("F_ang", F_ang);
-	imshow("LBP", F_lbp);
-	//imshow("Prueba", ANG);
-	*/
-	
-	
-	//DISPLAY_IMAGE_XY(false, F_hsv, 0 , 0);
-	//DISPLAY_IMAGE_XY(false, F_YCrCb, 1 , 0);
-	//DISPLAY_IMAGE_XY(false, F_lab, 2 , 0);
-	DISPLAY_IMAGE_XY(true, frame, 0 , 0);
-	cv::resizeWindow("frame", proc_W, proc_H);
-	//DISPLAY_IMAGE_XY(true, gray, 1 , 0);
-	DISPLAY_IMAGE_XY(true, F_hue, 1 , 0);
-	cv::resizeWindow("F_hue", 160, 120);
-	//cv::resizeWindow("F_hue", 160, 120);
-	DISPLAY_IMAGE_XY(true, F_sat, 2 , 0);
-	cv::resizeWindow("F_sat", 160, 120);
-	//cv::resizeWindow("F_sat", 160, 120);
-	//DISPLAY_IMAGE_XY(true, F_Cr, 5 , 0);
-	//DISPLAY_IMAGE_XY(true, F_Cb, 6 , 0);
-	//DISPLAY_IMAGE_XY(true, F_a, 7 , 0);
-	DISPLAY_IMAGE_XY(true, F_iic, 3 , 0);
-	cv::resizeWindow("F_iic", 160, 120);
-	DISPLAY_IMAGE_XY(true, F_mag, 4 , 0);
-	cv::resizeWindow("F_mag", 160, 120);
-	DISPLAY_IMAGE_XY(true, F_ang, 5 , 0);
-	cv::resizeWindow("F_ang", 160, 120);
-	DISPLAY_IMAGE_XY(true, F_lbp, 6 , 0);
-	cv::resizeWindow("F_lbp", 160, 120);
-}
-
-void CalculateMagnitudeOrientationOfGradients()
-{
-	Scharr(gray, temp_grad[0], gray.depth(), 1, 0, 1, 0, BORDER_DEFAULT);
-	convertScaleAbs(temp_grad[0], sobel[1], 1, 0);
-
-	Scharr(gray, temp_grad[1], gray.depth(), 0, 1, 1, 0, BORDER_DEFAULT);
-	convertScaleAbs(temp_grad[1], sobel[2], 1, 0);
-
-	Mat abs_grad_x = abs(temp_grad[0]);
-	Mat abs_grad_y = abs(temp_grad[1]);
-	F_mag = abs_grad_x + abs_grad_y;
-	F_mag = 255 - F_mag;
-	F_ang = 0*F_mag;
-	float result;
-	//ANG = atan2( temp_grad[1] ,temp_grad[0]);
-	for (int y = 0; y < temp_grad[1].rows; y++) 
-	{
-		for (int x = 0; x < temp_grad[1].cols; x++) 
-		{
-			float valueX = sobel[1].at<uchar>(y,x);
-			float valueY = sobel[2].at<uchar>(y,x);
-			float result = fastAtan2(valueY,valueX);
-			F_ang.at<uchar>(y, x) = (uchar)result;
-		}
-	}
-
-	normalize(F_mag, F_mag, 0, 255, CV_MINMAX);
-	convertScaleAbs(F_mag, F_mag, 1, 0); 
-	normalize(F_ang, F_ang, 0, 255, CV_MINMAX);
-	convertScaleAbs(F_ang, F_ang, 1, 0);	
-}
-
-
 void CalculateImageFeatures()
 {
 	cvtColor( frame, F_hsv, 	CV_BGR2HSV );	//Convert to HSV
@@ -277,7 +225,7 @@ void CalculateImageFeatures()
 
 	cv::split(F_lab,channel_3);
 	F_a=channel_3[1];
-	combine_channels(F_Cr, F_Cb, F_a, F_iic); //D = (A + B + 2*C)/4 //illumination invariant color channel combination
+	CalculateIIC(F_Cr, F_Cb, F_a, F_iic); //D = (A + B + 2*C)/4 //illumination invariant color channel combination
 	normalize(F_iic, F_iic, 0, 255, CV_MINMAX);
 
 	src=frame;
@@ -285,18 +233,18 @@ void CalculateImageFeatures()
 	
 	CalculateMagnitudeOrientationOfGradients();
 
-	F_lbp = LBP(gray);
+	F_lbp = CalculateLBP(gray);
 	
-	histogram(src,histImage); // Histogram Calculation
+	Histogram(src,histImage); // Histogram Calculation
 }
 
 
 void CameraSetup()
 {
 	cap = VideoCapture("eng_stat_obst.avi"); // Declare capture form
-	//VideoCapture cap(1);
+	//VideoCapture cap(0); //Camera integrada de la computadora
+	//VideoCapture cap(1); //Otra camara, conectada a la computadora mediante USB, por ejemplo.
 	
-	//InitializeImageVisualizers();
 	proc_W = 160;
 	proc_H = 120;
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
